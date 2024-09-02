5dd7027e55fdac77db4862bf92a1045d144a9dc4
Chirag Khatri <124987336+Mindgamer1024@users.noreply.github.com>
2024-09-02 22:56:25 +0530

    Delete icp_integration.h

--- icp_integration.h
+++
@@ -1,8 +0,0 @@
-#ifndef ICP_INTEGRATION_H
-#define ICP_INTEGRATION_H
-
-void connect_to_icp();
-void store_identity_on_icp(const char *username, const unsigned char *public_key);
-void verify_identity_on_icp(const char *username);
-
-#endif
