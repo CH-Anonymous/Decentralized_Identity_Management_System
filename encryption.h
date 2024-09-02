c262a146a74d6b30fcaa2c7fdf91c01adc1849ff
Chirag Khatri <124987336+Mindgamer1024@users.noreply.github.com>
2024-09-02 22:55:41 +0530

    Delete encryption.h

--- encryption.h
+++
@@ -1,8 +0,0 @@
-// encryption.h
-#ifndef ENCRYPTION_H
-#define ENCRYPTION_H
-
-void encrypt_data(unsigned char *plaintext, unsigned char *key, unsigned char *ciphertext);
-void decrypt_data(unsigned char *ciphertext, unsigned char *key, unsigned char *plaintext);
-
-#endif
